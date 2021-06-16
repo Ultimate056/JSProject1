f32635507978f3b305b7da56e7c7cf73a3c2f857
Valery <ugrass_060@mail.ru>
2021-06-16 10:06:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict"; // Директива (строгий режим)
+
+//console.log(1);
+// alert();
+let n = 10;
+let arr = ['plum.png', 2, 3, false]; // Массив
+// Объекты
+let obj = { 
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+// const result = confirm("Are you here?"); ОК - ОТМЕНА
+// const answer = prompt("Вам есть 18?", ""); ВВОД
+// 2 аргумент - дефолт ответ
+// РЕЗУЛЬТАТ - ВСЕГДА СТРОКА
+
+// const answer = +prompt("Вам есть18?", "");
+// СРАЗУ ПРЕОБРАЗУЕТ ДИНАМИЧЕСКИ В ЧИСЛО и т.д.
+
